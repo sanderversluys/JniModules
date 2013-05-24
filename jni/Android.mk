71bdfe6cd440202532afb210202bc3d6aa046c26
Sander Versluys <sversluys@reference.be>
2013-05-24 17:08:12 +0200

    Add project files

---
+++ jni/Android.mk
@@ -0,0 +1,8 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+include jni/libobj/Android.mk 
+include jni/libpng/Android.mk
+include jni/libzip/Android.mk
+include jni/app/Android.mk 
---
+++ obj/local/armeabi/objs/JniModules/JniModules.o.d
@@ -0,0 +1,46 @@
+obj/local/armeabi/objs/JniModules/JniModules.o: jni/app/JniModules.cpp \
+ /Users/sversluys/Development/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/jni.h \
+ /Users/sversluys/Development/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stdarg.h \
+ /Users/sversluys/Development/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stl/_cprolog.h \
+ /Users/sversluys/Development/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stl/config/features.h \
+ /Users/sversluys/Development/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stl/_stlport_version.h \
+ /Users/sversluys/Development/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stl/config/user_config.h \
+ /Users/sversluys/Development/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stl/config/compat.h \
+ /Users/sversluys/Development/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stl/config/host.h \
+ /Users/sversluys/Development/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stl/config/_system.h \
+ /Users/sversluys/Development/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stl/config/_android.h \
+ /Users/sversluys/Development/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stl/config/_gcc.h \
+ /Users/sversluys/Development/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stl/config/stl_confix.h \
+ /Users/sversluys/Development/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stl/config/_native_headers.h \
+ /Users/sversluys/Development/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stl/_epilog.h \
+ /Users/sversluys/Development/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stl/_config_compat_post.h
+
+/Users/sversluys/Development/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/jni.h:
+
+/Users/sversluys/Development/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stdarg.h:
+
+/Users/sversluys/Development/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stl/_cprolog.h:
+
+/Users/sversluys/Development/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stl/config/features.h:
+
+/Users/sversluys/Development/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stl/_stlport_version.h:
+
+/Users/sversluys/Development/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stl/config/user_config.h:
+
+/Users/sversluys/Development/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stl/config/compat.h:
+
+/Users/sversluys/Development/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stl/config/host.h:
+
+/Users/sversluys/Development/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stl/config/_system.h:
+
+/Users/sversluys/Development/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stl/config/_android.h:
+
+/Users/sversluys/Development/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stl/config/_gcc.h:
+
+/Users/sversluys/Development/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stl/config/stl_confix.h:
+
+/Users/sversluys/Development/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stl/config/_native_headers.h:
+
+/Users/sversluys/Development/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stl/_epilog.h:
+
+/Users/sversluys/Development/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stl/_config_compat_post.h:
