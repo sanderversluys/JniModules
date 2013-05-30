3807e0958625d4c7a793b251e7e8e629660b2cd6
Sander Versluys <sversluys@reference.be>
2013-05-30 18:37:47 +0200

    Fix submodule import

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
- 
+
+include jni/app/Android.mk  
 include jni/libpng/Android.mk
 include jni/libzip/Android.mk
-include jni/app/Android.mk 
--- jni/app/Android.mk
+++ jni/app/Android.mk
@@ -3,6 +3,11 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := JniModules
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../
+
 LOCAL_SRC_FILES := JniModules.cpp
+
+LOCAL_STATIC_LIBRARIES := libpng libzip
 
 include $(BUILD_SHARED_LIBRARY)
